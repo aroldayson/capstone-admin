5c62d2c3fd71767e8bd0aae11ed67037c337126f
Arold Ayson <aroldayson3@gmail.com>
2024-09-03 11:10:01 +0800

    updateds

--- src/app/Modules/price-managemt/view-pricemgt/view-pricemgt.component.ts
+++ src/app/Modules/price-managemt/view-pricemgt/view-pricemgt.component.ts
@@ -40,51 +40,51 @@ export class ViewPricemgtComponent implements OnInit {
   categid: any;
   users: any;
 
-  // updatebtn(item:any) {
-  //   console.log(item)
-  //   localStorage.setItem('id', item)
-  //   this.route.navigate(["/main/pricemanagementpage/pricemgtmain/pricemgtview/update"])
-  // }
+  updatebtn(item:any) {
+    console.log(item)
+    localStorage.setItem('id', item)
+    this.route.navigate(["/main/pricemanagementpage/pricemgtmain/pricemgtview/update"])
+  }
 
-  // dltbtn(id: any): void {
-  //   const swalWithBootstrapButtons = Swal.mixin({
-  //     customClass: {
-  //       confirmButton: 'btn btn-success mx-2',
-  //       cancelButton: 'btn btn-danger',
-  //     },
-  //     buttonsStyling: false,
-  //   });
-  //   swalWithBootstrapButtons
-  //     .fire({
-  //       title: 'Are you sure?',
-  //       text: "You won't be able to revert this!",
-  //       icon: 'warning',
-  //       showCancelButton: true,
-  //       confirmButtonText: 'Yes, delete it!',
-  //       cancelButtonText: 'No, cancel!',
-  //       reverseButtons: true,
-  //     })
-  //     .then((result) => {
-  //       if (result.isConfirmed) {
-  //         swalWithBootstrapButtons.fire({
-  //           title: 'Deleted!',
-  //           text: 'Your file has been deleted.',
-  //           icon: 'success',
-  //         });
-  //         this.admin.deleteprice(id).subscribe((result: any) => {
-  //           console.log('User Deleted!', result);
-  //           this.categ = result.data;
-  //         });
-  //       } else if (
-  //         /* Read more about handling dismissals below */
-  //         result.dismiss === Swal.DismissReason.cancel
-  //       ) {
-  //         swalWithBootstrapButtons.fire({
-  //           title: 'Cancelled',
-  //           text: 'Your imaginary file is safe :)',
-  //           icon: 'error',
-  //         });
-  //       }
-  //     });
-  // }
+  dltbtn(id: any): void {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success mx-2',
+        cancelButton: 'btn btn-danger',
+      },
+      buttonsStyling: false,
+    });
+    swalWithBootstrapButtons
+      .fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Yes, delete it!',
+        cancelButtonText: 'No, cancel!',
+        reverseButtons: true,
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          swalWithBootstrapButtons.fire({
+            title: 'Deleted!',
+            text: 'Your file has been deleted.',
+            icon: 'success',
+          });
+          this.admin.deleteprice(id).subscribe((result: any) => {
+            console.log('User Deleted!', result);
+            this.categ = result.data;
+          });
+        } else if (
+          /* Read more about handling dismissals below */
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          swalWithBootstrapButtons.fire({
+            title: 'Cancelled',
+            text: 'Your imaginary file is safe :)',
+            icon: 'error',
+          });
+        }
+      });
+  }
 }
--- src/app/Modules/staff/view-staff/view-staff.component.ts
+++ src/app/Modules/staff/view-staff/view-staff.component.ts
@@ -13,47 +13,48 @@ export class ViewStaffComponent implements OnInit{
   staff:any
 
   ngOnInit(): void {
-    // this.admin.displayitem().subscribe((result: any) => {
-    //   this.staff = result;
-    //   console.log(this.staff);
-    // });
+    this.admin.displayitem().subscribe((result: any) => {
+      this.staff = result;
+      console.log(this.staff);
+    });
   }
-  // constructor(
-  //   private admin: AdminService
-  // ){}
+  constructor(
+    private admin: AdminService
+  ){}
 
-  // dltbtn(): void {
-  //   const swalWithBootstrapButtons = Swal.mixin({
-  //     customClass: {
-  //       confirmButton: "btn btn-success mx-2",
-  //       cancelButton: "btn btn-danger"
-  //     },
-  //     buttonsStyling: false
-  //   });
-  //   swalWithBootstrapButtons.fire({
-  //     title: "Are you sure?",
-  //     text: "You won't be able to revert this!",
-  //     icon: "warning",
-  //     showCancelButton: true,
-  //     confirmButtonText:"Yes, delete it!",
-  //     cancelButtonText: "No, cancel!",
-  //     reverseButtons: true
-  //   }).then((result) => {
-  //     if (result.isConfirmed) {
-  //       swalWithBootstrapButtons.fire({
-  //         title: "Deleted!",
-  //         text: "Your file has been deleted.",
-  //         icon: "success"
-  //       });
-  //     } else if (
-  //       result.dismiss === Swal.DismissReason.cancel
-  //     ) {
-  //       swalWithBootstrapButtons.fire({
-  //         title: "Cancelled",
-  //         text: "Your imaginary file is safe :)",
-  //         icon: "error"
-  //       });
-  //     }
-  //   });
-  // }
+  dltbtn(): void {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-success mx-2",
+        cancelButton: "btn btn-danger"
+      },
+      buttonsStyling: false
+    });
+    swalWithBootstrapButtons.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText:"Yes, delete it!",
+      cancelButtonText: "No, cancel!",
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        swalWithBootstrapButtons.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success"
+        });
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        swalWithBootstrapButtons.fire({
+          title: "Cancelled",
+          text: "Your imaginary file is safe :)",
+          icon: "error"
+        });
+      }
+    });
+  }
 }
